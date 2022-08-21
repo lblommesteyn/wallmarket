458101fa10b6959a28237348663f77f7e1ac1091
lblommesteyn <61854440+lblommesteyn@users.noreply.github.com>
2022-08-20 23:07:54 -0400

    Add files via upload

---
+++ grocery_client.py
@@ -0,0 +1,34 @@
+import grpc
+import grocery.grocery_pb2_grpc as pb2_grpc
+import grocery.grocery_pb2 as pb2
+
+
+class GroceryClient(object):
+    """
+    Client for gRPC functionality
+    """
+
+    def __init__(self):
+        self.host = 'localhost'
+        self.server_port = 50051
+
+        # instantiate a channel
+        self.channel = grpc.insecure_channel(
+            '{}:{}'.format(self.host, self.server_port))
+
+        # bind the client and the server
+        self.stub = pb2_grpc.GroceryStub(self.channel)
+
+    def get_url(self, message):
+        """
+        Client function to call the rpc for GetServerResponse
+        """
+        message = pb2.Message(message=message)
+        print(f'{message}')
+        return self.stub.GetServerResponse(message)
+
+
+if __name__ == '__main__':
+    client = GroceryClient()
+    result = client.get_url(message="Hello Grocer you there?")
+    print(f'{result}')
---
+++ grocery_server.py
@@ -0,0 +1,32 @@
+import grpc
+from concurrent import futures
+import time
+import grocery.grocery_pb2_grpc as pb2_grpc
+import grocery.grocery_pb2 as pb2
+
+
+class GroceryService(pb2_grpc.GroceryServicer):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def GetServerResponse(self, request, context):
+
+        # get the string from the incoming request
+        message = request.message
+        result = f'Hello I am up and running received "{message}" message from you'
+        result = {'message': result, 'received': True}
+
+        return pb2.MessageResponse(**result)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_GroceryServicer_to_server(GroceryService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
